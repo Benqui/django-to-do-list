5d23bbb3051d301ebc86b0de89e99061344d1ed7
Benqui <bennyhoyos@hotmail.com>
2024-02-05 18:50:57 -0600

    creacion del modelo para las tasks

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    TODO = "TO DO"
+    COMPLETED = "COMPLETED"
+    INPROGRESS = "IN-PROGRESS"
+    STATUS_CHOICES = [ (TODO, 'TO DO'),(COMPLETED,'COMPLETED'),(INPROGRESS, 'IN-PROGRESS') ]
+    
+    title = models.CharField(max_length = 50)
+    description = models.TextField(null=True,blank=True)
+    date_created = models.DateTimeField(auto_now_add = True)
+    due_date = models.DateField(null=True,blank=True)
+    status = models.CharField(max_length= 50, choices=STATUS_CHOICES, null=True, blank=True,default= TODO)
+    def __str__(self) -> str:
+        return self.title
